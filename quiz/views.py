ae99977042ada14f2aa0aa817175d29d67f138dd
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2024-07-12 16:42:03 +0300

    added login functionality

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import auth
+from django.contrib import messages
 # Create your views here.
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login') 
+    return render(request, 'auth/login.html')
