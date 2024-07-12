41960e6102512105764e2069ac4bda4006d00b64
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2024-07-12 17:27:59 +0300

    Register User

--- quiz/views.py
+++ quiz/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import auth
 from django.contrib import messages
+
+from .models import User
 # Create your views here.
 @login_required(login_url='login')
 def home(request):
-    return render(request, 'home.html')
+    user = request.user
+    return render(request, 'home.html', {'user': user})
 
 def login(request):
     if request.method == 'POST':
@@ -20,3 +23,27 @@ def login(request):
             messages.info(request, 'invalid credentials')
             return redirect('login') 
     return render(request, 'auth/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(email=email, password=password, first_name=first_name, last_name=last_name)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('register')
+    return render(request, 'auth/register.html')
