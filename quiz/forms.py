760d05e4d1066ec07233bbbc8b7b296492be77a9
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2024-07-12 15:18:22 +0300

    Created custom user model to allow user to authenticate with their emails

---
+++ quiz/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from .models import User
+
+class UserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email',)
+
+class UserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = ('email',)
--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
     }
 }
 
+# Use Custom User Class
+AUTH_USER_MODEL = 'quiz.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
