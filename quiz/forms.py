f341f90011f6feb019df6d079ffda915908145b4
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2024-07-13 22:38:11 +0300

    forms for quiz, question and answer

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
-from .models import User
+from .models import *
 
 class UserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -11,3 +11,30 @@ class UserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
         fields = ('email',)
+
+class QuizForm(forms.ModelForm):
+    class Meta:
+        model = Quiz
+        fields = ('name', 'description')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('question_text',)
+        widgets = {
+            'question_text': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ('answer_text', 'is_correct')
+        widgets = {
+            'answer_text': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_correct': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
