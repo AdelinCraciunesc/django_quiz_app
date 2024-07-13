101c57b80ee679936feb7331f7142655e60d6288
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2024-07-14 00:45:01 +0300

    routing for the pages

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,7 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # auth
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('logout/', views.logout, name='logout'),   
+    path('logout/', views.logout, name='logout'),
+    # quiz
+    path('quiz/<int:quiz_id>/', views.quiz_page, name='quiz_page'),
+    path('your_quizzes/', views.your_quizzes, name='your_quizzes'),
+    path('create_quiz/', views.create_quiz, name='create_quiz'),
+    path('delete_quiz/<int:quiz_id>/', views.delete_quiz, name='delete_quiz'),
+    path('edit_quiz/<int:quiz_id>/', views.edit_quiz, name='edit_quiz'),
+    path('question_add/<int:quiz_id>/', views.question_add, name='question_add'),
+    path('question_delete/<int:question_id>/', views.question_delete, name='question_delete'),
+    path('question_edit/<int:question_id>/', views.question_edit, name='question_edit'),
 ]
