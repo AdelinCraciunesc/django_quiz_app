6ce370595160c52dc219cb615848b8fec85fbe5f
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2024-07-13 22:30:45 +0300

    Define models for quiz, question and answer

--- quiz/models.py
+++ quiz/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 from .managers import UserManager
 
-# Create your models here.
+# User model with custom manager and fields so we can login with email
 class User(AbstractUser):
     username = None
     first_name = models.CharField(max_length=100, blank=True, default='')
@@ -12,3 +12,21 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = UserManager()
+
+# Quiz model with fields for name and description and user that created it
+# TODO add category
+class Quiz(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Question model with fields for quiz and question text
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+
+# Answer model with fields for question and answer text and whether it is correct
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField()
