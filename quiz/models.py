b096244729eb0915392852da853a4c947faab637
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2024-07-31 19:01:39 +0300

    added category model, and category field to quiz

--- quiz/models.py
+++ quiz/models.py
@@ -14,16 +14,26 @@ class User(AbstractUser):
     objects = UserManager()
 
 # Quiz model with fields for name and description and user that created it
-# TODO add category
+# TODO add dynamic timer
 class Quiz(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    
+# Category model with fields for name
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    # override the __str__ method to return the name instead of the object
+    def __str__(self):
+        return self.name
 
 # Question model with fields for quiz and question text
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
+    score = models.IntegerField(default=1)
 
 # Answer model with fields for question and answer text and whether it is correct
 class Answer(models.Model):
@@ -34,4 +44,4 @@ class Answer(models.Model):
 class User_Result(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    score = models.IntegerField()
+    total_score = models.IntegerField(default=0)
